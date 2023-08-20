aa9f317f3fc6f1363fdc90c007b77bb8ee4b5881
A. A. Badmus <enamux.aa@gmail.com>
2023-08-20 10:05:43 -0700

    function

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_putchar - prins _putchar to std
+ *
+ * Return: on success 0
+ */
+
+int print_putchar(void)
+{
+	char c[] =  "_putchar";
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+	_putchar(c[i]);
+	}
+	return (0);
+}
+
