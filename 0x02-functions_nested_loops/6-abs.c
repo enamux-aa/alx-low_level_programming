9cde4d0a45c741095b6064d0d0dacea4f229ac92
A. A. Badmus <enamux.aa@gmail.com>
2023-08-20 14:43:54 -0700

    Absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute number ogf n
+ *
+ * @n: the parameter to be tested
+ *
+ * Return: 1
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-(n));
+	}
+	else
+	{
+		return (n);
+	}
+}
+
