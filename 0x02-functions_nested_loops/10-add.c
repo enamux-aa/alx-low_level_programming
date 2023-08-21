58d0e5a0277933a2a88e565c08b795d2611bd32b
A. A. Badmus <enamux.aa@gmail.com>
2023-08-21 13:10:26 -0700

    10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - check the code.
+ * add two integers and return the value
+ * @c: number
+ * @d: number
+ *
+ * Return: Always 0.
+ */
+
+int add(int c, int d)
+{
+	return (c + d);
+}
