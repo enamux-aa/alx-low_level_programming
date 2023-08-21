09de18767a7bae93cb085474fb4677504dd74e32
A. A. Badmus <enamux.aa@gmail.com>
2023-08-20 23:44:34 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,28 +1,25 @@
  #include "main.h"
 /**
- * print_last_digit- to print the last digist of number
+ * print_last_digit- to print the last digit of number
  *
  * @n: the number to tesr
  *
- * Return : the last digit
+ * Return: the last digit
  */
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	if (n > 0)
+	if (n >= 0)
 	{
-		_putchar (last_digit);
+		_putchar ('0' + last_digit);
 		return (last_digit);
-	}
-	else if (n == 0)
-	{
-		_putchar (last_digit);
-		return (0);
 	}
 	else
 	{
-		_putchar(-(last_digit));
-		return (-(n % 10));
+		last_digit = -(last_digit);
+		_putchar ('0' + last_digit);
+		return (last_digit);
 	}
 }
+
